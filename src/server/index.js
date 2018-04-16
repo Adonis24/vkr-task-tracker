1575567bff1e0472f66123e64e71a55d4dd9dca8
Sergey Bukharov <s.buharov@dodopizza.com>
2018-04-16 20:50:34 +0300

    empty page, static files

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs')
+
 app.get('/', (request, response) => {
-    response.json({
-        started: true
-    })
+    response.render('index')
 })
 
 app.listen(port, () => {
