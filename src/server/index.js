b8749dc5dbed334f8be535b196d2a18325489261
Sergey Bukharov <s.buharov@dodopizza.com>
2018-04-18 19:10:55 +0300

    hardcoede tasks

--- src/server/index.js
+++ src/server/index.js
@@ -2,15 +2,49 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
-app.use(express.static(path.join(__dirname, '/public')))
-
-app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs')
 
+app.set('views', path.join(__dirname, 'views'));
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.get('/', (request, response) => {
-    response.render('index')
+    const plannedTasks = [
+        {
+            title: 'task 1',
+            description: 'task 1',
+            status: 'todo'
+        },
+        {
+            title: 'task 2',
+            description: 'task 2',
+            status: 'todo'
+        },
+    ]
+
+    const inPorgressTasks = [
+        {
+            title: 'task 3',
+            description: 'task 3',
+            status: 'wip'
+        }
+    ]
+
+    const finishedTasks = [
+        {
+            title: 'task 4',
+            description: 'task 4',
+            status: 'done'
+        }
+    ]
+
+
+    response.render('index', {
+        plannedTasks: plannedTasks,
+        inProgressTasks: inPorgressTasks,
+        finishedTasks: finishedTasks
+    })
 })
 
 app.listen(port, () => {
